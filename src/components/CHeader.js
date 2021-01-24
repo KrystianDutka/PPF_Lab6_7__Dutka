08ec13c24b7b48dacebe609fa805c52b84ab27d0
Krystian Dutka <krystian.dutka@gmail.com>
2021-01-24 18:30:23 +0100

    React-01-wstęp

---
+++ src/components/CHeader.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import logo from '../logo.svg';
+import './CHeader.css'
+class CHeader extends Component {
+    render(){
+        return(
+        <div className="headerContainer">
+            <img src={logo} className="headerLogo" alt="logo"/>
+            <h2>Witaj w przykładowym serwsie {this.props.aName+ ' ' +this.props.aFamily}  </h2>
+        </div>
+        );
+        }
+    }
+
+    export default CHeader
