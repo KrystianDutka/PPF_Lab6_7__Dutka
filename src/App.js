da9ef668f01d4a02c9765ee5a504ffb8a4b155ce
Krystian Dutka <krystian.dutka@gmail.com>
2021-01-24 18:52:38 +0100

    PPF_Lab6_7_Wlasne

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
+import React, {Component} from 'react';
 import './App.css';
-import {CFooter} from './components/CFooter'
-import CHeader from './components/CHeader'
-import CContents from './components/CContents'
-import COuter from './components/COuter'
+import CHeader from './components/CHeader.js';
+import CMain from './components/CMain';
+import CVideo from './components/CVideo.js';
+import CEvents from './components/CEvents';
+import CEvents2 from './components/CEvents2';
+import CCWC from './components/CCWC.js';
+import KDK from './components/KDK.js'
+import CFormularz from './components/CFormularz.js'
+import CFooter from './components/CFooter.js';
+
 
 function App() {
-  return (
+return (
     <div className="App">
-      <CHeader aName="Krystian" aFamily="Dutka"/>
-      {/*<CContents/>*/}
-      <COuter/>
-      <CFooter weather="słoneczna" temp="25">
-      <p>Niebo jest bezchmurne, wiatr słaby</p> 
-      </CFooter> 
-    </div>
-  );
-}
+    <CHeader  info1="Strona główna" info2="Kategorie wagowe" info3="Historia boksu" info4="Mistrzowie" />
+    <CMain/>  
+    <CVideo/>
+    <CEvents/>
+    <CEvents2/>
+    <CCWC/>
+    <KDK/>
+    <CFormularz/>
+    <CFooter name="Krystiana" surname="Dutka"/> 
+      </div>
+    );
+  }
+
 
 export default App;
--- src/components/CFooter.js
+++ src/components/CFooter.js
@@ -1,36 +1,16 @@
-import React from 'react';
-import './CFooter.css';
-//function CFooter(){
-//    let date = new Date();
-//   return (
-//    <div>
-//    <h5>PPFront-end, przykładowy serwis React. Dzisiaj mamy: {date.toDateString()}</h5>
-//    </div>    
-//    );
-//}
+import React, {Component} from 'react';
+import './CFooter.css'
 
-export const CFooter = (props) => {
-   const {weather, temp, children} = props;
-   let date = new Date();
-   return (
-   <div>
-   <h5>PPFront-end, przykładowy serwis React. Dzisiaj mamy: {date.toDateString()}
-   <br/>
-   Stan pogody: {weather}, temperatura: {temp}.
-   <br/>
-   {children} 
-   </h5>
-   </div>    
-   );
+class CFooter extends Component {
+    render () {
+        return (
+            <div>
+                <h5 className='fonts'>
+                    Strona internetowa stworzona przez {this.props.name} {this.props.surname}
+                </h5>
+            </div>    
+        );  
+    }
 }
- 
-// export const CFooter = () => {
-//     let date = new Date();
-//     return React.createElement('div', {className: 'footerDivClass'}, 
-//     React.createElement('h5',{id: 'footerText', className: 'footerTextClass'},
-//     'PPFront-end, przykładowy serwis React. Dzisiaj mamy: ' +date.toDateString()));
 
-// }
- 
- 
-//export default CFooter
+export default CFooter
